38ebb460e28d5097a25c2c6eeea569a562e50d0b
Alex Pshul <alex@pshul.com>
2021-12-19 11:49:48 +0200

    Add project files.

---
+++ GeekJokeFetcher/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace GeekJokeFetcher
+{
+	public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _http = new HttpClient();
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void FetchJoke(object sender, EventArgs e)
+        {
+            GeekJoke.Text = "Fetching a new joke...";
+
+			HttpResponseMessage response = await _http.GetAsync("https://geek-jokes.sameerkumar.website/api?format=text");
+            GeekJoke.Text = await response.Content.ReadAsStringAsync();
+
+			SemanticScreenReader.Announce(GeekJoke.Text);
+		}
+	}
+}
